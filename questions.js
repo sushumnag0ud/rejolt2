6e25fd86b5949ff07c043f2f7b0f69cd23b3b3d4
Sushumna <113690334+sushumnag0ud@users.noreply.github.com>
2023-01-07 17:50:16 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Full form of APK?",
+    answer: "Android Package Kit",
+    options: [
+      "Application Package Kit",
+      "Android Package Kit",
+      "Application programming kit",
+      "Android programming kit"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who won 2022 Fifa world cup?",
+    answer: "Argentina",
+    options: [
+      "Brazil",
+      "Portugal",
+      "France",
+      "Argentina"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Brand ambassador of Audi?",
+    answer: "Virat Kohli",
+    options: [
+      "Deepika Padukone",
+      "Virat Kohli",
+      "Anushka Sharma",
+      "MS Dhoni"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is not a web browser?",
+    answer: "file explorer",
+    options: [
+      "chrome",
+      "edge",
+      "firefox",
+      "file explorer"
+    ]
+  },
+    {
+    numb: 5,
+    question: "what is the capital city of Nigeria",
+    answer: "Abuja",
+    options: [
+      "Abuja",
+      "Dhaka",
+      "Cairo",
+      "Nairobia"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
